c27c8ac6a52bd1139b729e85e7be9cd1aefb747a
Ricardo Salgado <ricardodsalg@gmail.com>
2024-12-22 17:43:30 -0500

    Finishing the Project.
    IT'S LIVE!!

--- js/script.js
+++ js/script.js
@@ -3,7 +3,35 @@ const multiForm = document.querySelector('#multiplication-tab');
 const numberInput = document.querySelector('#number');
 const multiplyInput = document.querySelector('#multiplicator');
 
+const multiplicationTitle = document.querySelector('#chosen-nbr');
+const multiplicationTable = document.querySelector('#multiplication-operations');
 // functions
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for(i = 1; i <= multiplicatorNumber; i++) {
+
+        const result = number * i
+
+        const template = 
+            `<div class="row">
+                <div class="operation">${number} x ${i}</div>
+                <div class="result-mlt">&nbsp; = ${result}</div>
+            </div>`
+
+        const parser = new DOMParser()
+
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+
+    }
+
+    multiplicationTitle.innerText = ` ${number}`
+
+};
 
 // events
 
@@ -15,5 +43,5 @@ multiForm.addEventListener('submit', (e) => {
 
     if(!multiplicationNumber || !multiplicatorNumber) return;
 
-    console.log(multiplicationNumber, multiplicatorNumber)
+    createTable(multiplicationNumber, multiplicatorNumber)
 })
